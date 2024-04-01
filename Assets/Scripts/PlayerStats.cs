632d4f8ae45ed8e8d31b70fe10dfc5cf958ce329
Cole Andrews <colegamedesign@gmail.com>
2024-04-01 17:59:39 -0400

    Few changes during my class to death UI still very WIP and much is broken.

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
 using DigitalMedia.Combat;
 using DigitalMedia.Core;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 namespace DigitalMedia
@@ -11,8 +13,15 @@ namespace DigitalMedia
     public class PlayerStats : StatsComponent
     {
 
-        [SerializeField] private GameObject deathUI;
+        [SerializeField] private GameObject dyingUi;
+        [SerializeField] private GameObject deadUI;
         private PlayerCombatSystem _combatSystem;
+
+
+        private PlayerInput _playerInput;
+        private InputAction reload;
+        private InputAction die;
+        private int respawn = -1;
         
         private void Start()
         {
@@ -29,8 +38,27 @@ namespace DigitalMedia
             _combatSystem = GetComponent<PlayerCombatSystem>();
 
             rb = this.gameObject.GetComponent<Rigidbody2D>();
+            
+            _playerInput = GetComponent<PlayerInput>();
+            reload = _playerInput.actions["Reload"];
+            die = _playerInput.actions["Die"];
+            reload.performed += PlayerWantsTolive;
+            die.performed += PlayerWantsToDie;
         }
+
         
+        private void OnEnable()
+        {
+            reload.Enable();
+            die.Enable();
+        }
+
+        private void OnDisable()
+        {
+            reload.Dispose();
+            die.Dispose();
+        }
+
         // Update is called once per frame
         public override void DealDamage(float incomingDamage, GameObject attackOrigin, float knockbackForce = .5f, bool interruptAction = true)
         {
@@ -84,7 +112,7 @@ namespace DigitalMedia
             {
                 HandleLives();
                 /*Time.timeScale = 0f;
-                deathUI.SetActive(true);*/
+                dyingUi.SetActive(true);*/
             }
         }
 
@@ -94,30 +122,80 @@ namespace DigitalMedia
             currentLives -= 1;
             if (currentLives <= 0)
             { 
-                 PlayerRespawn();
+                Debug.Log("The player has died.");
+                deadUI.SetActive(true);
+                deadUI.GetComponent<Animator>()?.Play("Default");
+                PlayerRespawn();
             }
-            
-            
-            health = data.BasicData.maxHealth;
-            vitality = data.BasicData.maxVitality;
-            vitalityBar.value = 1;
-            healthbar.value = 1;
-            vitalityBar.GetComponent<Slider>().fillRect.GetComponent<Image>().color = new Color(0, 1, 0.5437737f, 1);
-            
-            //2. Update UI to match lives remaining.
-            livesUI.ElementAt(currentLives).SetActive(false);
-            
-            //3. Re-enabling enemy states if they were attacking -- or just turn their BTs back on. 
-            InitateStateChange(State.Idle);
-            
-            //4. Check if there are any lives remaining and destroy the object if needed. Theoretically we may need to do it first.
 
+            Time.timeScale = 0;
+            PlayerReboot();
         }
 
+        private void PlayerReboot()
+        {
+            _playerInput.SwitchCurrentActionMap("Dying");
+            respawn = -1;
+            
+            dyingUi.SetActive(true);
+
+            StartCoroutine(WaitForDecision());
+            
+            dyingUi.SetActive(false);
+            
+            //Player wants to die
+            if (respawn == 0)
+            {
+                Debug.Log("The player has lost a life and chose to die.");
+
+                deadUI.SetActive(true);
+                deadUI.GetComponent<Animator>()?.Play("Default");
+                PlayerRespawn();
+            }
+            else if (respawn == 1) //Player Wants to live
+            {
+                Debug.Log("The player has lost a life and chose to revive.");
+
+                health = data.BasicData.maxHealth;
+                vitality = data.BasicData.maxVitality;
+                vitalityBar.value = 1;
+                healthbar.value = 1;
+                vitalityBar.GetComponent<Slider>().fillRect.GetComponent<Image>().color = new Color(0, 1, 0.5437737f, 1);
+            
+                //2. Update UI to match lives remaining.
+                livesUI.ElementAt(currentLives).SetActive(false);
+            
+                //3. Re-enabling enemy states if they were attacking -- or just turn their BTs back on. 
+                InitateStateChange(State.Idle);
+                _playerInput.SwitchCurrentActionMap("Player");
+            }
+        }
+
+        private void PlayerWantsToDie(InputAction.CallbackContext context)
+        {
+            respawn = 0;
+        }
+        private void PlayerWantsTolive(InputAction.CallbackContext context)
+        {
+            respawn = 1;
+        }
+        private IEnumerator WaitForDecision()
+        {
+            respawn = -1;
+            while (respawn == -1)
+            {
+                
+            }
+            
+            yield return null;
+            
+        }
         public void PlayerRespawn()
         {
-            Time.timeScale = 0f;
             //Handle respawning 
+            //Player needs to return to the checkpoint and start with full health and stats. 
+            //For now we can just load the main scene again. 
+            SceneManager.LoadScene("Main");
         }
     }
 }
